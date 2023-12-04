1fe8e2f0486ee446866af0442490a0695b231ffd
digholkhan <digholkhan@gmail.com>
2023-12-04 14:47:41 +0600

    modified

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -12,6 +12,8 @@ import finsweet from '../../assets/projects/Finsweet.png'
 import PDFTool from '../../assets/projects/PDFTools.png'
 import Innovate from '../../assets/projects/innovate.png'
 import Qurier from '../../assets/projects/qurierbox.png'
+import Finix from '../../assets/projects/Finix.png'
+import Sublime from '../../assets/projects/Sublime.png'
 import {FaAngleRight} from 'react-icons/fa'
 import {FaAngleLeft} from 'react-icons/fa'
 
@@ -103,14 +105,14 @@ const Projects = () => {
                 <Project ImgSrc={Innovate} CodeSrc='https://github.com/Digholkhan/innovate' liveSrc='https://digholkhan.github.io/innovate/' PName='Innovate'/>
             </div>
             <div>
-                <Project ImgSrc={ecommerce} CodeSrc="https://github.com/Digholkhan/E-commerce"  liveSrc='https://orebiecommerce.netlify.app/' PName='Orebi E commerce'/>
+                <Project ImgSrc={Finix} CodeSrc="https://github.com/Digholkhan/finix"  liveSrc='https://digholkhan.github.io/finix/#' PName='Finix'/>
             </div> 
             <div>
-                <Project ImgSrc={avada} CodeSrc='https://github.com/Digholkhan/Extreme-Sports' liveSrc='https://exsports.netlify.app/' PName='Avada Extreme Sports'/>
+                <Project ImgSrc={Sublime} CodeSrc='https://github.com/Digholkhan/Extreme-Sports' liveSrc='https://exsports.netlify.app/' PName='Sublime'/>
             </div>  
             <div>
-               <Project ImgSrc={PDFTool} CodeSrc='https://github.com/Digholkhan/PDF-TOOLS' liveSrc='https://pdftools-sampledesign.netlify.app/' PName='PDF Tools'/>  
-            </div>   
+                <Project ImgSrc={ecommerce} CodeSrc="https://github.com/Digholkhan/E-commerce"  liveSrc='https://orebiecommerce.netlify.app/' PName='Orebi E commerce'/>
+            </div>  
            
            
         </Slider>
